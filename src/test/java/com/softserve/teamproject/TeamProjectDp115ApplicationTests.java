a9b671261b913070444e8912a6159d3e4a8db4f0
dev1 <dev1>
2017-07-07 01:27:59 +0300

    first commit

---
+++ src/test/java/com/softserve/teamproject/TeamProjectDp115ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softserve.teamproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamProjectDp115ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
